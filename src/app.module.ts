ca596f44b184f28a2fdfbbb58ddb696c099b8862
borel-T <borel-T@users.noreply.github.com>
2024-02-18 23:40:50 +0300

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule],
+  imports: [UsersModule, AuthModule, PrismaModule, EmailModule],
   controllers: [],
   providers: [],
 })
