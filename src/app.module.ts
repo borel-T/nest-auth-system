d64b476e741fe3c73c9d6c508446f929845f4b24
borel-T <borel-T@users.noreply.github.com>
2024-02-19 14:06:13 +0300

    added event emitter module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { EmailModule } from './email/email.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [UsersModule, AuthModule, PrismaModule, EmailModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    PrismaModule,
+    EmailModule,
+    EventEmitterModule.forRoot(), //initializes the event emitter and registers any declarative event listeners that exist within your app
+  ],
   controllers: [],
   providers: [],
 })
