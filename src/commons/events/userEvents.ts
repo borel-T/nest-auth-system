2fa288b4e7093f4a716f48aea50717b09eda492a
borel-T <borel-T@users.noreply.github.com>
2024-02-20 02:17:09 +0300

    added events for event driven programming

---
+++ src/commons/events/userEvents.ts
@@ -0,0 +1,7 @@
+// user events
+export const ACCOUNT_CREATED = 'account.created';
+export const ACCOUNT_ACTIVATED = 'account.activated';
+export const ACCOUNT_PASSWORD_FORGOTTEN = 'account.passwordforgotten';
+export const ACCOUNT_PASSWORD_UPDATED = 'account.password.updated';
+export const ACCOUNT_DELETED = 'account.deleted';
+export const ACCOUNT_BLOCKED = 'account.blocked';
